f8332c4c1dc62a9c43f8dea21b2396b457f8d66c
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-22 14:14:17 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist
+    attr_accessor :name 
+
+    # def initialize(name)
+    # @name= name 
+    # end
+
+end 
+
+# beyonce = Artist.new("Beyonce")
+# singsong=Song.new("singsong")
+# singsong.artist= beyonce 
